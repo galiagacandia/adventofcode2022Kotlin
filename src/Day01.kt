b2a36f7616f86a68670a398cdefb491ab14a3968
Grover Leopoldo Aliaga Candia <galiagacandia@gmail.com>
2022-12-01 22:43:54 -0400

    Avances Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(calories: List<String>): Int {
+        var contadorElfo: Int = 0;
+        var totalCalories: Int = 0
+        var maximasCalories: Int = 0
+        var elfoConMasCalories: String = ""
+        var mapElfosCalories = mutableMapOf<Int, Int>()
+        for (calorie in calories) {
+            println(calorie)
+            if (calorie.isNotEmpty()){
+                totalCalories += calorie.toInt();
+            } else {
+                contadorElfo++
+                // Vacia llenamos la suma a una variable
+                if(totalCalories > maximasCalories){
+                    maximasCalories = totalCalories
+                    elfoConMasCalories = contadorElfo.toString()
+                    totalCalories = 0
+                    mapElfosCalories[contadorElfo] = maximasCalories
+                }
+            }
+        }
+        //calories.forEach { s -> s.toBigInteger() }.
+        return maximasCalories
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +30,11 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    var resultado: Int = part1(testInput)
+    println("Maxima: " + resultado)
+    //check(part1(testInput) == 14)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    //val input = readInput("Day01")
+    //println(part1(input))
+    //println(part2(input))
 }
